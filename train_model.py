e5cf2016cfe4fae5194654a5c2d99d2e3a38c926
arminstr <armin.straller@gmail.com>
2021-07-01 13:49:26 +0200

    added gradient accumulation

--- train_model.py
+++ train_model.py
@@ -16,7 +16,6 @@ tf.config.optimizer.set_experimental_options({'layout_optimizer': False})
 from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
 
-
 from convert_single_scenario import tf_example_scenario
 from data_generator import DataDirectoryStorage, DataGenerator
 
@@ -38,36 +37,92 @@ TIME_STEPS_100MS = 30
 # Since only one second of history is provided by waymo, the parameter is set to 10 frames each representing 100 ms of historical data.
 NUMBER_OF_FRAMES = 10
 
+# -----------------------
+# Currently only trainable with batch size one in Nvidia 1060 with 6GB of graphics memory.
+# This is due to the memory required for intermediate calculations e.g. layer activation outputs.
+# The required amount of memory increases linearly with batch size.
+
+# there are two main solutions to this problem. 
+# - Increasing Amount of Memory available by using graphics card with more memory available
+# - splitting up the data into smaller batches and train those on different GPUs in parallel or 
+#   sequentially on one GPU
+
+# References:
+# https://towardsdatascience.com/how-to-break-gpu-memory-boundaries-even-with-large-batch-sizes-7a9c27a400ce
+# https://towardsdatascience.com/what-is-gradient-accumulation-in-deep-learning-ec034122cfa
+
+# Gradient Accumulation
+# This is a technique using smaller batches and a sequential feed to the GPU.
+# The linked article describes an implementation of gradient accumulation for keras models using runai.
+# https://towardsdatascience.com/how-to-easily-use-gradient-accumulation-in-keras-models-fa02c0342b60
+# STEPS defines the number of steps the gradients are accumulated over
+# -----------------------
+
+batch_size = 1
+GRADIENT_ACCUMULATION_STEPS = 128
+
+# training 100 epochs this would take ~ 100 * 10h on my setup
+epochs = 100
+
+# init tf session
 tf.keras.backend.clear_session()
 if tf.config.list_physical_devices('GPU'):
     physical_devices = tf.config.list_physical_devices('GPU')
     tf.config.experimental.set_memory_growth(physical_devices[0], enable=True)
     # tf.config.experimental.set_virtual_device_configuration(physical_devices[0], [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=6000)])
 
-seq = tf.keras.Sequential(
-    [
-        tf.keras.Input(
-            shape=(NUMBER_OF_FRAMES, GRID_SIZE, GRID_SIZE, 1), dtype="float32"
-        ),
-        layers.ConvLSTM2D(
-            filters=128, kernel_size=(5, 5), padding="same", return_sequences=True
-        ),
-        layers.BatchNormalization(),
-        layers.ConvLSTM2D(
-            filters=128, kernel_size=(3, 3), padding="same", return_sequences=True
-        ),
-        layers.BatchNormalization(),
-        layers.ConvLSTM2D(
-            filters=128, kernel_size=(1, 1), padding="same", return_sequences=True
-        ),
-        layers.BatchNormalization(),
-        layers.Conv3D(
-            filters=1, kernel_size=(3, 3, 3), activation="sigmoid", padding="same"
-        ),
-    ]
-)
+# Custom Train Step Class from 
+# https://stackoverflow.com/questions/66472201/gradient-accumulation-with-custom-model-fit-in-tf-keras
+# This enables gradient accumulation
+class CustomTrainStep(tf.keras.Model):
+    def __init__(self, n_gradients, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.n_gradients = tf.constant(n_gradients, dtype=tf.int32)
+        self.n_acum_step = tf.Variable(0, dtype=tf.int32, trainable=False)
+        self.gradient_accumulation = [tf.Variable(tf.zeros_like(v, dtype=tf.float32), trainable=False) for v in self.trainable_variables]
 
+    def train_step(self, data):
+        self.n_acum_step.assign_add(1)
 
+        x, y = data
+        # Gradient Tape
+        with tf.GradientTape() as tape:
+            y_pred = self(x, training=True)
+            loss = self.compiled_loss(y, y_pred, regularization_losses=self.losses)
+        # Calculate batch gradients
+        gradients = tape.gradient(loss, self.trainable_variables)
+        # Accumulate batch gradients
+        for i in range(len(self.gradient_accumulation)):
+            self.gradient_accumulation[i].assign_add(gradients[i])
+ 
+        # If n_acum_step reach the n_gradients then we apply accumulated gradients to update the variables otherwise do nothing
+        tf.cond(tf.equal(self.n_acum_step, self.n_gradients), self.apply_accu_gradients, lambda: None)
+
+        # update metrics
+        self.compiled_metrics.update_state(y, y_pred)
+        return {m.name: m.result() for m in self.metrics}
+
+    def apply_accu_gradients(self):
+        # apply accumulated gradients
+        self.optimizer.apply_gradients(zip(self.gradient_accumulation, self.trainable_variables))
+
+        # reset
+        self.n_acum_step.assign(0)
+        for i in range(len(self.gradient_accumulation)):
+            self.gradient_accumulation[i].assign(tf.zeros_like(self.trainable_variables[i], dtype=tf.float32))
+
+# Model 
+input = tf.keras.Input(shape=(NUMBER_OF_FRAMES, GRID_SIZE, GRID_SIZE, 1), dtype="float32")
+base_maps = tf.keras.layers.ConvLSTM2D(filters=128, kernel_size=(5, 5), padding="same", return_sequences=True)(input)
+base_maps = tf.keras.layers.BatchNormalization()(base_maps)
+base_maps = tf.keras.layers.ConvLSTM2D(filters=128, kernel_size=(3, 3), padding="same", return_sequences=True)(base_maps)
+base_maps = tf.keras.layers.BatchNormalization()(base_maps)
+base_maps = tf.keras.layers.ConvLSTM2D(filters=128, kernel_size=(1, 1), padding="same", return_sequences=True)(base_maps)
+base_maps = tf.keras.layers.BatchNormalization()(base_maps)
+base_maps = tf.keras.layers.Conv3D(filters=1, kernel_size=(3, 3, 3), activation="sigmoid", padding="same")(base_maps)
+custom_model = CustomTrainStep(n_gradients=GRADIENT_ACCUMULATION_STEPS, inputs=[input], outputs=[base_maps])
+
+# custom loss function focusing on bright pixels which represent objects
 def only_bright_pixels_custom(y_true, y_pred):
  
     threshold = tf.reduce_max(y_true) / 2
@@ -89,119 +144,42 @@ def only_bright_pixels_custom(y_true, y_pred):
 
     return tf.reduce_mean(squared_difference_vehicle, axis=1) * 0.9 + tf.reduce_mean(squared_difference_map, axis=1) * 0.1
 
-seq.compile(loss=only_bright_pixels_custom, optimizer="adadelta", metrics=['mse'])
-
-def load_scenarios(n_samples, path_name):
-    """
-    load scenarios that were saved using convert_training_data.py
-    """
-    n_frames=10
-    frames = np.zeros((n_samples, n_frames, GRID_SIZE, GRID_SIZE, 1), dtype=np.float32)
-    label_frames = np.zeros((n_samples, n_frames, GRID_SIZE, GRID_SIZE, 1), dtype=np.float32)
-    _, directories, _ = next(walk(path_name))
-    i = 0
-    sample_index = 0
-    for directory in directories:
-        if 'static_' in directory: 
-            _, _, filenames = next(walk(path_name + '/' + directory))
-            temp_frames = np.zeros((DATA_LENGTH, GRID_SIZE, GRID_SIZE, 1), dtype=np.float32)
-            for filename in filenames:
-                image = imageio.imread(path_name + '/' + directory + '/' + filename, as_gray=True)
-                image = np.reshape(image, (GRID_SIZE, GRID_SIZE,1))
-                filename_sections = filename.split('_')
-                filename_index, _ = filename_sections[-1].split('.')
-                temp_frames[int(filename_index)] = image/255
-
-            # start at index one since frame 0 is empty
-            frames[sample_index] = temp_frames[0:n_frames]
-            label_frames[sample_index] = temp_frames[1:n_frames+1]
-
-            sample_index += 1
-            if sample_index >= n_samples:
-                return frames, label_frames
-            i += 1
-
-epochs = 200
-
-
-# -----------------------
-# Currently only trainable with batch size one in Nvidia 1060 with 6GB of graphics memory.
-# This is due to the memory required for intermediate calculations e.g. layer activation outputs.
-# The required amount of memory increases linearly with batch size.
-
-# there are two main solutions to this problem. 
-# - Increasing Amount of Memory available by using graphics card with more memory available
-# - splitting up the data into smaller batches and train those on different GPUs in parallel or 
-#   sequentially on one GPU
-
-# References:
-# https://towardsdatascience.com/how-to-break-gpu-memory-boundaries-even-with-large-batch-sizes-7a9c27a400ce
-# https://towardsdatascience.com/what-is-gradient-accumulation-in-deep-learning-ec034122cfa
-
-# -----------------------
-batch_size = 1
-
-
-# n_samples_train = 1000
-# n_samples_val = 150
-
-# past_frames_train, label_frames_train = load_scenarios(n_samples_train, PATHNAME_TRAINING)
-# past_frames_validation, label_frames_validation = load_scenarios(n_samples_val, PATHNAME_VALIDATION)
-
-# print("past frames", past_frames_train[:n_samples_train].shape)
-# print("label frames", label_frames_train[:n_samples_train].shape)
-seq.summary()
+# Compile the custom model
+custom_model.compile(loss=only_bright_pixels_custom, optimizer="adadelta", metrics=['mse'])
+# print the model summary
+custom_model.summary()
 
 # Include the epoch in the file name (uses `str.format`)
 checkpoint_path = "training_checkpoints/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S") + "/cp-{epoch:04d}.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
-
 log_dir = "./logs/fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
 # Create a callback that saves the model's weights
-seq_callbacks = [
+custom_model_callbacks = [
     tf.keras.callbacks.ModelCheckpoint( filepath=checkpoint_path, 
                                         verbose=0, 
                                         save_weights_only=True, 
-                                        save_freq=batch_size*10),
+                                        save_freq=GRADIENT_ACCUMULATION_STEPS*batch_size),
     tf.keras.callbacks.TensorBoard(     log_dir=log_dir, 
-                                        update_freq=batch_size,
+                                        update_freq=GRADIENT_ACCUMULATION_STEPS*batch_size,
                                         histogram_freq=1),
 ]
 
-# This may generate warnings related to saving the state of the optimizer.
-# These warnings (and similar warnings throughout this notebook)
-# are in place to discourage outdated usage, and can be ignored.
-
-seq.save_weights(checkpoint_path.format(epoch=0))
+# saving weights
+custom_model.save_weights(checkpoint_path.format(epoch=0))
 
 # generate data ids for data generation the format is defined in DataGenerator
-
 data_info_training = DataDirectoryStorage(PATHNAME_TRAINING, 'static_tfrecord-', TIME_STEPS_100MS, NUMBER_OF_FRAMES)
 training_generator = DataGenerator(data_info_training, (NUMBER_OF_FRAMES, GRID_SIZE, GRID_SIZE, 1), batch_size)
 
 data_info_validation = DataDirectoryStorage(PATHNAME_VALIDATION, 'static_tfrecord-', TIME_STEPS_100MS, NUMBER_OF_FRAMES)
 validation_generator = DataGenerator(data_info_validation, (NUMBER_OF_FRAMES, GRID_SIZE, GRID_SIZE, 1), batch_size)
 
-
-# seq.fit(
-#     # TODO: Enable training with data generator
-#     past_frames_train,
-#     label_frames_train,
-#     shuffle=True,
-#     batch_size=batch_size,
-#     epochs=epochs,
-#     verbose=1,
-#     validation_data = (past_frames_validation, label_frames_validation),
-#     # validation_split=0.1,
-#     callbacks=seq_callbacks
-# )
-
 # Train model on dataset
-seq.fit(
+custom_model.fit(
     training_generator,
     validation_data=validation_generator,
     epochs=epochs,
-    callbacks=seq_callbacks,
+    callbacks=custom_model_callbacks,
     use_multiprocessing=True,
     workers=6)
